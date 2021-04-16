e00edb3c927cb8b1ff6342e6a394617731055230
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-04-16 09:37:45 +0200

    setting up auth package

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('about', function () {
     return view('about');
 });
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
