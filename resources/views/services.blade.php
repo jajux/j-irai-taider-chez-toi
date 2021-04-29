a3ac014c119d9c47622728103b7a9da657b63f5c
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-04-29 16:50:10 +0200

    update front pages services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -6,47 +6,36 @@
         <p>Description des services proposés et les modalités.</p>
     </x-slot>
     <div class="container shadow-lg">
-
         <div>
             <h1 class="text-center pt-5 text-capitalize">j'irai t'aider chez toi</h1>
-            <p class="card-text text-center mb-5"><small class="text-muted">Ces services vous sont proposés et éxécutés par l'association <strong><a href="https:://garros-service.fr" class="text-dark" target="_blank">Garros Services</a></strong></small></p>
+                 <p class="card-text text-center mb-5">
+                    <small class="text-muted">Ces services vous sont proposés et éxécutés par l'association.
+                        <strong>
+                            <a href="https::/garros-service.fr" class="text-dark" target="_blank">Garros Services</a>
+                        </strong>
+                    </small>
+                </p>
         </div>
-
-
-
-        <div class="card mb-3 h-100">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <img src="images\bricoleurs.png" class="img-fluid" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">bricolage</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
+    </div>    
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-12 col-md-6 order-2 order-lg-1">
+                <img src="images\bricoleurs.png" class="img-fluid" alt="...">
+            </div>
+            <div class="col-12 col-md-6 order-2 order-lg-2">
+                <h1 class="col-12 col-md-5 order-1 order-lg-2">Bricolage</h1>
+                <p class="lead">Vous désirez effectuer des réparations ou des améliorations dans votre logement.</p>
+                <p>L'association de régie de quartier Garros Services vous offre la possibilité de réserver ce services depuis cette plateforme.</p>
+            </div>
+            <div class="">
+                <a href="{{route('reservations')}}" class="btn btn-success">Cliquez pour réserver un service bricolage</a>
             </div>
         </div>
-
-        <div class="card mb-3 h-100">
-            <div class="row no-gutters">
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">assistance numérique</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="col-md-4 img-fluid">
-                    <img src="images\assistance_numerique.png" class="img-fluid" alt="...">
-                </div>
-            </div>
-        </div>
-
-
     </div>
+    <div class="pattern-bottom"></div>
+
+
+
+
 </x-app-layout>
 @include('incs.footer')
