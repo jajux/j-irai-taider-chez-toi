6d786ead2c6334f77c7937624f344254b8159b14
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-05-01 07:38:27 +0200

    update components & email verification

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,4 @@
 {{-- logo place on  dashboard & user profile --}}
 <a class="d-flex justify-content-center mb-4" href="/"  {{ $attributes }}>
-    <img  width="100" src="images/J-irai_t-aider_chez_toi_logo.png" alt="Modification de profile">
+    <img  width="100" src="{{asset('images/J-irai_t-aider_chez_toi_logo.png')}}" alt="Modification de profile">
 </a>
